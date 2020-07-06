3e7f5dad8f19ca9f5dfca1a36fffa2a90b141727
MVBPinho <marcos.vb.pinho@live.com>
2020-07-05 23:32:16 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-content = {"from":"broken-clownfish","to":"5511951922834","contents":[{"type":"text","text":"Lucas noob1"}]};
+content = {"from":"broken-clownfish","to":"","contents":[{"type":"text","text":"Vitoria noob1"}]};
 
 request.post({
     headers: {'content-type' : 'application/json', 'X-API-TOKEN': 'dekG603V4x7FoAwcCF3Y2XMPSmcB9iBqOWKi'},
